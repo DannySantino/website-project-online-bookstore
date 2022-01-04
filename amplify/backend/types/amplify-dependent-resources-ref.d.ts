1d5479c7032429dff5e964d19f53c874553471e0
Danny Santino <dannysantisimo@gmail.com>
2022-01-04 16:18:06 +0100

    push resources to cloud

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,40 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "onlinebookstorebd6df218": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "BookImages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "processPayment": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "createOrder": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "onlinebookstore": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/graphql/queries.js
@@ -0,0 +1,117 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getOrder = /* GraphQL */ `
+  query GetOrder($id: ID!) {
+    getOrder(id: $id) {
+      id
+      user
+      date
+      total
+      books {
+        items {
+          id
+          book_id
+          order_id
+          createdAt
+          updatedAt
+          customer
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      customer
+    }
+  }
+`;
+export const listOrders = /* GraphQL */ `
+  query ListOrders(
+    $id: ID
+    $filter: ModelOrderFilterInput
+    $limit: Int
+    $nextToken: String
+    $sortDirection: ModelSortDirection
+  ) {
+    listOrders(
+      id: $id
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      sortDirection: $sortDirection
+    ) {
+      items {
+        id
+        user
+        date
+        total
+        books {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        customer
+      }
+      nextToken
+    }
+  }
+`;
+export const getBook = /* GraphQL */ `
+  query GetBook($id: ID!) {
+    getBook(id: $id) {
+      id
+      title
+      description
+      image
+      author
+      featured
+      price
+      orders {
+        items {
+          id
+          book_id
+          order_id
+          createdAt
+          updatedAt
+          customer
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listBooks = /* GraphQL */ `
+  query ListBooks(
+    $id: ID
+    $filter: ModelBookFilterInput
+    $limit: Int
+    $nextToken: String
+    $sortDirection: ModelSortDirection
+  ) {
+    listBooks(
+      id: $id
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      sortDirection: $sortDirection
+    ) {
+      items {
+        id
+        title
+        description
+        image
+        author
+        featured
+        price
+        orders {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
